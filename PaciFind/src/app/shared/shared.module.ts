84eb93abe32e59d5e0715b1229b2c4c152f67adb
AlexandraS <alexandrastan1171@gmail.com>
2020-06-03 04:29:00 +0300

    Steregere componente nenecesare
    Am sters componentele pentru descrierea doctorului si pentru container-ul paginii doctorului

--- PaciFind/src/app/shared/shared.module.ts
+++ PaciFind/src/app/shared/shared.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DoctorProfileContainerComponent } from './doctor-profile-container/doctor-profile-container.component';
 import { ContactContainerComponent } from './contact-container/contact-container.component';
 import { AllPacientsPageContainerComponent } from './all-pacients-page-container/all-pacients-page-container.component';
 import { AllDoctorsPageContainerComponent } from './all-doctors-page-container/all-doctors-page-container.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { DoctorDescriptionComponent } from './doctor-description/doctor-description.component';
 import { PacientsListComponent } from './pacients-list/pacients-list.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { DoctorsListComponent } from './doctors-list/doctors-list.component';
@@ -15,8 +13,6 @@ import { DoctorsListComponent } from './doctors-list/doctors-list.component';
 @NgModule({
   declarations: [
     SidebarComponent, 
-    DoctorProfileContainerComponent, 
-    DoctorDescriptionComponent, 
     ContactContainerComponent, 
     AllPacientsPageContainerComponent, 
     PacientsListComponent, 
@@ -28,7 +24,6 @@ import { DoctorsListComponent } from './doctors-list/doctors-list.component';
     CommonModule,
   ],
   exports:[
-    DoctorProfileContainerComponent,
     ContactContainerComponent,
     AllPacientsPageContainerComponent,
     AllDoctorsPageContainerComponent,
